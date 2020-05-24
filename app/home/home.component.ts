fdc09c5226fe382e22ba3eead8a696559a91b157
anurag <singh.anurag.v@gmail.com>
2020-05-24 10:23:06 +0530

    added icons and sourcing news list in cache

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -4,6 +4,18 @@ import { NewsModel } from "../model/news-model";
 import { NewsSourcesModel } from "../model/news-sources-model";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import * as utils from "tns-core-modules/utils/utils";
+import {
+    getBoolean,
+    setBoolean,
+    getNumber,
+    setNumber,
+    getString,
+    setString,
+    hasKey,
+    remove,
+    clear
+} from "tns-core-modules/application-settings";
+import { SwipeDirection } from "@nativescript/core/ui/gestures/gestures";
 
 @Component({
     selector: "Home",
@@ -38,12 +50,35 @@ export class HomeComponent implements OnInit {
         utils.openUrl(url);
     }
     constructor(private http: HttpClient) {
+        if (hasKey("newsList")) {
+            this.newsList = JSON.parse(getString("newsList"));
+            this.count = getNumber("count");
+        }
+        if (hasKey("sourceList")) {
+            this.sourceList = JSON.parse(getString("sourceList"));
+            this.sourcesCount = getNumber("sourcesCount");
+        }
     }
 
     ngOnInit(): void {
-        this.loadNews();
-        this.loadSources();
-        // 
+        if (this.newsList.length === 0) {
+            this.loadNews();
+        }
+        if (this.sourceList.length === 0) {
+            this.loadSources();
+        }
+    }
+    swipe(args, source) {
+        if (args.direction == SwipeDirection.down) {
+            switch (source) {
+                case 'newsList':
+                this.loadNews();
+                break;
+                case 'sourceList':
+                this.loadSources();
+                break;
+            }
+        }
     }
     onSearch() {
         this.http.get("https://newsapi.org/v2/everything?apiKey=cbcde33bf59b4c799c2860fd2c55b934&q=" + this.textFieldValue).subscribe((response) => {
@@ -55,12 +90,16 @@ export class HomeComponent implements OnInit {
         this.http.get(this.serverUrl).subscribe((response) => {
             this.newsList = response['articles'];
             this.count = response['totalResults'];
+            setString("newsList", JSON.stringify(this.newsList));
+            setNumber("count", this.count);
         });
     }
     loadSources() {
         this.http.get("https://newsapi.org/v2/sources?apiKey=cbcde33bf59b4c799c2860fd2c55b934").subscribe((response) => {
             this.sourceList = response['sources'];
             this.sourcesCount = this.sourceList.length;
+            setString("sourceList", JSON.stringify(this.sourceList));
+            setNumber("sourcesCount", this.sourcesCount);
         });
     }
 }
